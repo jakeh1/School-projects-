f52b8afd949d5b927e31364dafe4db67f4629ee2
Charles Roltgen <roltgenc@uwplatt.edu>
2020-10-09 12:59:05 -0500

    Update Example.java
    Fixed capitalization

--- Example.java
+++ Example.java
@@ -1,7 +1,7 @@
 class Example{
     public static void main(String args[]){
-        System.out.println("hello world!");
-        System.out.println("Good bye world");
+        System.out.println("Hello World!");
+        System.out.println("Good bye World");
         
     }
 }
